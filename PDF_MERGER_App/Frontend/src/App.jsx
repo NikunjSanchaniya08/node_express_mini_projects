2474aefb1c121abb6b712a092e68e193efb89518
nikunj <nikunjsanchaniya08@gmail.com>
2025-09-09 23:35:22 +0530

    Setup React frontend with Bootstrap

---
+++ PDF_MERGER_App/Frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
